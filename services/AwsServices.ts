93f9049b3df47f32a97b17709a6581b049f30555
diogoizzo <dizzodev@gmail.com>
2023-10-24 12:04:50 -0300

    alterando aws para deletar

--- services/AwsServices.ts
+++ services/AwsServices.ts
@@ -1,21 +1,20 @@
 import { randomUUID } from 'crypto';
-import { S3 } from '@aws-sdk/client-s3';
-
-const region = process.env.REGION;
-const bucketName = process.env.BUCKET_NAME;
-const accessKeyId = process.env.ACCESS_KEY;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-
-const bucket = new s3({
-   apiVersion: '2006-03-01',
-   region,
-   accessKeyId,
-   secretAccessKey,
-   signatureVersion: 'v4'
-});
+import s3 from 'aws-sdk/clients/s3';
 
 export default class AwsServices {
    static async generateUrl(awsFileName: string) {
+      const region = process.env.REGION;
+      const bucketName = process.env.BUCKET_NAME;
+      const accessKeyId = process.env.ACCESS_KEY;
+      const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+      const bucket = new s3({
+         apiVersion: '2006-03-01',
+         region,
+         accessKeyId,
+         secretAccessKey,
+         signatureVersion: 'v4'
+      });
       const params = {
          Bucket: bucketName,
          Key: awsFileName,
@@ -25,9 +24,25 @@ export default class AwsServices {
       return url;
    }
    static async delete(awsFileName: string) {
-      const deleted = await bucket
-         .deleteObject({ Bucket: String(bucketName), Key: awsFileName })
-         .promise();
-      return deleted;
+      const region = process.env.REGION;
+      const bucketName = process.env.BUCKET_NAME;
+      const accessKeyId = process.env.ACCESS_KEY;
+      const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+      const bucket = new s3({
+         apiVersion: '2006-03-01',
+         region,
+         accessKeyId,
+         secretAccessKey,
+         signatureVersion: 'v3'
+      });
+      try {
+         const deleted = await bucket
+            .deleteObject({ Bucket: String(bucketName), Key: awsFileName })
+            .promise();
+         return deleted;
+      } catch (error) {
+         console.log(error);
+      }
    }
 }
