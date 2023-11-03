74dec1deab8e3a0c5ccd257a505bd42e8ed9eae0
diogoizzo <dizzodev@gmail.com>
2023-11-03 17:24:31 -0300

    página de perguntas frequentes finalizada

--- pages/perguntas/index.tsx
+++ pages/perguntas/index.tsx
@@ -18,10 +18,11 @@ function Faq() {
          <main className="bg-why-gray-100 py-16">
             <div className="container px-4 mx-auto">
                <div className="max-w-5xl mx-auto ">
-                  <h2 className="text-why-blue-900 text-3xl md:text-5xl mb-6  font-medium">
+                  <h2 className="text-why-blue-900 text-3xl md:text-5xl mb-3  font-medium">
                      Dúvidas Gerais
                   </h2>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 mb-3 shadow-md p-6">
+                  <div className="h-[1px] bg-why-blue-900 mb-8 opacity-30"></div>
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer bg-why-gray-50 border border-white mb-3 shadow-md p-6">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Para quem é o Rio Travel Club?
@@ -43,13 +44,13 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         Para todo mundo que deseja visitar o Rio de Janeiro e
                         conhecer as melhores atrações e pontos turísticos
                         economizando dinheiro.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg border border-white cursor-pointer bg-why-gray-50 shadow-md p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Como o Rio Travel Club Funciona?
@@ -71,7 +72,7 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         O Rio Travel Club foi desenhando para te ajudar a
                         economizar em sua viagem para o Rio de Janeiro. Buscamos
                         parcerias com as melhores agências turísticas e atrações
@@ -79,7 +80,7 @@ function Faq() {
                         qualidade com preços mais acessíveis.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg border border-white cursor-pointer bg-why-gray-50 shadow-md p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Eu preciso fazer reserva para os passeios e atrações?
@@ -101,14 +102,14 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         Sim. Após virar membro do Rio Travel Club, você terá que
                         escolher o seu passeio ou atração e realizar a reserva
                         para o dia desejado, através da nossa área de membros ou
                         do nosso suporte dedicado.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md mb-10 p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg border border-white cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Ao entrar no clube eu terei direto a utilizar todas
@@ -131,16 +132,17 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         Sim. Ao assinar o Rio travel club você ganha acesso a
                         todas as ofertas de atrações e passeios no Rio de
                         Janeiro.
                      </p>
                   </details>
-                  <h2 className="text-why-blue-900 block text-3xl md:text-5xl mt-12 mb-6   font-medium">
+                  <h2 className="text-why-blue-900 block text-3xl md:text-5xl mt-12 mb-3   font-medium">
                      Pagamento
                   </h2>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
+                  <div className="h-[1px] bg-why-blue-900 mb-8 opacity-30"></div>
+                  <details className="group font-roboto text-lg rounded-lg border border-white cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Quanto custa o Rio Travel Club?
@@ -162,12 +164,12 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         O valor original do Rio Travel club é de R$ 200,00 por
                         pessoa. Em algumas épocas temos promoções de desconto.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg border border-white cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            O pagamento é seguro?
@@ -189,13 +191,13 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         Sim. Usamos o sistema de pagamento do Mercado Pago, uma
                         empresa já consolidada e segura para receber pagamentos
                         por cartão.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg border border-white cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            A assinatura é mensal ou paga apenas um vez?
@@ -217,13 +219,13 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         A assinatura paga-se apenas uma vez e já pode sair
                         utilizando todas ofertas disponíveis na região
                         selecionada. Não fazemos renovações automáticas.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Por quanto tempo terei acesso ao Rio Travel Club?
@@ -245,15 +247,16 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         O Rio travel club tem a validade de 12 meses a partir da
                         compra.
                      </p>
                   </details>
-                  <h2 className="text-why-blue-900 block text-3xl md:text-5xl mt-12 mb-6   font-medium">
+                  <h2 className="text-why-blue-900 block text-3xl md:text-5xl mt-12 mb-3   font-medium">
                      Dúvidas de Uso
                   </h2>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
+                  <div className="h-[1px] bg-why-blue-900 mb-8 opacity-30"></div>
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Como faço para reservar um passeio ou experiência?
@@ -275,7 +278,7 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         Após entrar para Rio Travel Club, você terá acesso a uma
                         área de membros com todas as suas informações e nossos
                         contatos para reservas. <br /> Basta mandar uma mensagem
@@ -285,7 +288,7 @@ function Faq() {
                         valor possível por aquela experiência.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Como faço para saber mais informações sobre o passeio
@@ -308,7 +311,7 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         Após você se tornar membro do Rio Travel Club, basta
                         entrar em contato com nosso suporte de reservas através
                         do nosso Whatsapp ou e-mail que te ajudamos a esclarecer
@@ -316,7 +319,7 @@ function Faq() {
                         parceiro.
                      </p>
                   </details>
-                  <details className="group font-roboto md:text-xl rounded-lg cursor-pointer bg-why-gray-50 shadow-md  p-6 mb-3">
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
                      <summary className="flex items-center justify-between  ">
                         <h3 className="font-medium text-why-gray-900">
                            Posso reservar um passeio ou atração do Rio Travel
@@ -339,10 +342,128 @@ function Faq() {
                         </svg>
                      </summary>
 
-                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 md:text-xl">
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
                         Não. Na hora da compra do Rio Travel Club você terá que
                         informar o nome da pessoa titular e os descontos serão
                         aplicados somente para essa pessoa.
+                     </p>
+                  </details>
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
+                     <summary className="flex items-center justify-between  ">
+                        <h3 className="font-medium text-why-gray-900">
+                           Estou viajando em 2 ou mais pessoas, preciso comprar
+                           um Rio Travel Club para cada pessoa?
+                        </h3>
+
+                        <svg
+                           className="flex-shrink-0 ml-1.5 w-5 h-5 md:h-7 md:w-7 transition duration-300 group-open:-rotate-180"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           stroke="#333333"
+                        >
+                           <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M19 9l-7 7-7-7"
+                           />
+                        </svg>
+                     </summary>
+
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
+                        Sim. Se todos querem economizar em sua viagem, é
+                        necessária cada pessoa virar membro do Rio Travel Club
+                        para ter acesso aos descontos.
+                     </p>
+                  </details>
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
+                     <summary className="flex items-center justify-between  ">
+                        <h3 className="font-medium text-why-gray-900">
+                           O pagamento do passeio ou atração é feito
+                           antecipadamente para o Rio Travel Club?
+                        </h3>
+
+                        <svg
+                           className="flex-shrink-0 ml-1.5 w-5 h-5 md:h-7 md:w-7 transition duration-300 group-open:-rotate-180"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           stroke="#333333"
+                        >
+                           <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M19 9l-7 7-7-7"
+                           />
+                        </svg>
+                     </summary>
+
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
+                        Não. Os pagamentos dos passeios ou atrações são feitos
+                        diretamente para nossos parceiros. Para algumas
+                        experiências pode ser exigido um pagamento de entrada
+                        para a reserva do passeio, mas essa entrada também será
+                        paga diretamente para nosso parceiro.
+                     </p>
+                  </details>
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
+                     <summary className="flex items-center justify-between  ">
+                        <h3 className="font-medium text-why-gray-900">
+                           Posso pagar o passeio ou atração com cartão?
+                        </h3>
+
+                        <svg
+                           className="flex-shrink-0 ml-1.5 w-5 h-5 md:h-7 md:w-7 transition duration-300 group-open:-rotate-180"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           stroke="#333333"
+                        >
+                           <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M19 9l-7 7-7-7"
+                           />
+                        </svg>
+                     </summary>
+
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
+                        Os valores dos passeios e atrações divulgados no Rio
+                        Travel Club são para pagamentos por pix ou dinheiro. A
+                        maioria dos parceiros aceitam também o pagamento no
+                        cartão com uma pequena taxa de acréscimo, inclusive com
+                        a opção de parcelamento.
+                     </p>
+                  </details>
+                  <details className="group font-roboto text-lg rounded-lg cursor-pointer border border-white bg-why-gray-50 shadow-md  p-6 mb-3">
+                     <summary className="flex items-center justify-between  ">
+                        <h3 className="font-medium text-why-gray-900">
+                           Posso presentear alguém com o Rio Travel Club?
+                        </h3>
+
+                        <svg
+                           className="flex-shrink-0 ml-1.5 w-5 h-5 md:h-7 md:w-7 transition duration-300 group-open:-rotate-180"
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           stroke="#333333"
+                        >
+                           <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M19 9l-7 7-7-7"
+                           />
+                        </svg>
+                     </summary>
+
+                     <p className=" mt-4 leading-relaxed text-justify text-why-gray-800 text-lg">
+                        Claro. Após a sua compra, você será redirecionado para o
+                        cadastro em nossa área de membro, basta realizar o
+                        cadastro com os dados da pessoa presenteada.
                      </p>
                   </details>
                </div>
